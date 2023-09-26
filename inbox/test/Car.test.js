ef19b8a0614ba90b5ad679ca1ccea4cdc1cf0548
Danthesage01 <ayenibabajidedaniel@gmail.com>
2023-09-26 23:03:01 +0100

    update

--- inbox/test/Car.test.js
+++ inbox/test/Car.test.js
@@ -1,44 +1,44 @@
-// const ganache = require("ganache");
-// const { Web3 } = require("web3");
-// const assert = require("assert");
-// const web3 = new Web3(ganache.provider());
+const ganache = require("ganache");
+const { Web3 } = require("web3");
+const assert = require("assert");
+const web3 = new Web3(ganache.provider());
 
-// // updated ganache and web3 imports added for convenience
+// updated ganache and web3 imports added for convenience
 
-// // contract test code will go here
-// class Car {
-//   park() {
-//     return "stopped";
-//   }
-//   drive() {
-//     return "vroom";
-//   }
-// }
+// contract test code will go here
+class Car {
+  park() {
+    return "stopped";
+  }
+  drive() {
+    return "vroom";
+  }
+}
 
-// let car;
-// beforeEach(() => {
-//   car = new Car();
-// });
+let car;
+beforeEach(() => {
+  car = new Car();
+});
 
-// describe("Car Class", () => {
-//   it("car can park", () => {
-//     assert.equal(car.park(), "stopped");
-//   });
-//   it("car can drive", () => {
-//     assert.equal(car.drive(), "vroom");
-//   });
-// });
-// // beforeEach(()=>{
-// //  console.log("test")
-// // })
+describe("Car Class", () => {
+  it("car can park", () => {
+    assert.equal(car.park(), "stopped");
+  });
+  it("car can drive", () => {
+    assert.equal(car.drive(), "vroom");
+  });
+});
+// beforeEach(()=>{
+//  console.log("test")
+// })
 
-// // describe("Car Class", ()=>{
-// // it("car can park", ()=>{
-// //  const car = new Car();
-// //  assert.equal(car.park(), "stopped")
-// // } )
-// // it("car can drive", ()=>{
-// //  const car = new Car();
-// //  assert.equal(car.drive(), "vroom")
-// // })
-// // })
+// describe("Car Class", ()=>{
+// it("car can park", ()=>{
+//  const car = new Car();
+//  assert.equal(car.park(), "stopped")
+// } )
+// it("car can drive", ()=>{
+//  const car = new Car();
+//  assert.equal(car.drive(), "vroom")
+// })
+// })
